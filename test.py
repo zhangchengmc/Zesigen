8a090eeac18f8239e41603c94bf33cdb99473ee6
zhangchengmcbedrock <120537551+zhangchengmcbedrock@users.noreply.github.com>
2024-02-09 10:00:55 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 import os
 import threading
 import webbrowser
+import datetime
 
 def get_filename_from_url(url):
     # 从下载链接中提取文件名.
@@ -61,38 +62,49 @@ def download():
         else:
             break
 
+    # 判断是否是4月1日
+    current_date = datetime.date.today()
+    is_april_fools_day = current_date.month == 4 and current_date.day == 1
+
     # 是否使用多线程下载
     use_multithread = input("是否使用多线程下载？(是/否): ").lower() in ['是', 'yes', '是的', 'y']
 
-    if use_multithread:
-        # 输入多线程下载的线程数
-        thread_count = int(input("请输入多线程下载的线程数："))
-
-        print("开始下载...")
-        threads = []
-
-        for url in urls:
-            if url == "-mickey-mouse":
-                open_url_in_browser("https://luotianyimiku.github.io/R-C.jpg")
-            else:
-                save_path = choose_save_location(get_filename_from_url(url))
-                thread = threading.Thread(target=download_single_file, args=(url, save_path))
-                threads.append(thread)
-                thread.start()
-
-        for thread in threads:
-            thread.join()
-
+    if is_april_fools_day:
+        print("今天是愚人节，程序即将闪退...")
     else:
-        print("开始单线程下载...")
-        for url in urls:
-            if url == "-mickey-mouse":
-                open_url_in_browser("https://luotianyimiku.github.io/R-C.jpg")
-            else:
-                save_path = choose_save_location(get_filename_from_url(url))
-                download_single_file(url, save_path)
+        if use_multithread:
+            # 输入多线程下载的线程数
+            thread_count = int(input("请输入多线程下载的线程数："))
 
-    print("所有文件下载完成！")
+            print("开始下载...")
+            threads = []
+
+            for url in urls:
+                if url == "Zesigen-Download-Manager-github":
+                    open_url_in_browser("https://github.com/zhangchengmc/Zesigen-Download-Manager")
+                elif url == "Zesigen-Download-Manager-debug":
+                    print("触发调试模式，程序即将闪退...")
+                else:
+                    save_path = choose_save_location(get_filename_from_url(url))
+                    thread = threading.Thread(target=download_single_file, args=(url, save_path))
+                    threads.append(thread)
+                    thread.start()
+
+            for thread in threads:
+                thread.join()
+
+        else:
+            print("开始单线程下载...")
+            for url in urls:
+                if url == "Zesigen-Download-Manager-github":
+                    open_url_in_browser("https://github.com/zhangchengmc/Zesigen-Download-Manager")
+                elif url == "Zesigen-Download-Manager-debug":
+                    print("触发调试模式，程序即将闪退...")
+                else:
+                    save_path = choose_save_location(get_filename_from_url(url))
+                    download_single_file(url, save_path)
+
+        print("所有文件下载完成！")
 
 if __name__ == '__main__':
     download()
